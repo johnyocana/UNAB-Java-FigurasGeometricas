9e6ba860934f25509117667565b99ba03929928d
JAOC <JAOC@DESKTOP-SKA2Q8R>
2021-08-24 17:58:52 -0500

    UNAB Ciclo 2 Java - Figuras geometricas

---
+++ src/unab/figuras/Calculo.java
@@ -0,0 +1,11 @@
+package unab.figuras;
+
+
+public class Calculo {
+
+    public static void main(String[] args) throws Exception {
+        Funciones f = new Funciones();
+        f.menu();
+
+    }
+}
